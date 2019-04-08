2ce98a06d9c566227623ae2f2b30cbfff1bcb33c
Nijtus <47044864+Nijtus@users.noreply.github.com>
2019-04-08 10:24:33 +0200

    ajout des header

---
+++ Emprunt_add.php
@@ -0,0 +1,16 @@
+<?php
+include("connexion_bdd.php");
+// traitement
+if(isset($_POST)  )  // si il existe certaines variables dans le tableau associatif $_POST
+{                    // le formulaire vient d'être soumis
+
+}
+
+// affichage de la vue
+?>
+<?php include("v_head.php");  ?>
+<?php include ("v_nav.php");  ?>
+
+<!-- affichage(vue) relatif à la page -->
+
+<?php include ("v_foot.php");  ?>
