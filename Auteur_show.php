a2febe4d8503a3d4c81d753c77c6c25a56a71776
Dhovabear <43112786+Dhovabear@users.noreply.github.com>
2019-04-12 19:06:30 +0200

    reecriture de Auteur_show car perdu avec les bugs de git sur les pc de l'iut

--- Auteur_show.php
+++ Auteur_show.php
@@ -3,7 +3,8 @@ include("connexion_bdd.php");
 // traitement
 if(isset($_POST)  )  // si il existe certaines variables dans le tableau associatif $_POST
 {                    // le formulaire vient d'être soumis
-
+    $commande = "SELECT * FROM AUTEUR;";
+    $auteurs = $bdd->query($commande)->fetchAll();
 }
 
 // affichage de la vue
@@ -11,6 +12,28 @@ if(isset($_POST)  )  // si il existe certaines variables dans le tableau associa
 <?php include("v_head.php");  ?>
 <?php include ("v_nav.php");  ?>
 
+<div class="row">
+    <a href="Auteur_add.php">Ajouter un auteur</a>
+    <?php if(isset($auteurs[0])): ?>
+        <table border="2">
+            <caption>Liste des auteurs</caption>
+            <thead>
+                <tr><th>nom de l'auteur</th><th>prenom de l'auteur</th><th>actions</th></tr>
+            </thead>
+            <tbody>
+                <?php foreach ($auteurs as $ligne ): ?>
+                    <tr>
+                        <td><?php echo($ligne["nomAuteur"]); ?></td>
+                        <td><?php echo($ligne["prenomAuteur"]); ?></td>
+                        <td><a href="Auteur_delete.php">Supprimer</a></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <div class="erreur">Aucun auteur enregistré dans la base de donnée</div>
+    <?php endif; ?>
+</div>
 <!-- affichage(vue) relatif à la page -->
 
 <?php include ("v_foot.php");  ?>
