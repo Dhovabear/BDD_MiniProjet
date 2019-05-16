4107600e775e6245073fe19bbb679d7b4ef14f5c
Dhovabear <powerkiki25000@gmail.com>
2019-05-16 08:59:28 +0200

    Modifs de oeuvres show et fini debut de l'emprunt

---
+++ Exemplaire_delete.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kvuillau
+ * Date: 16/05/19
+ * Time: 08:33
+ */
--- Exemplaire_show.php
+++ Exemplaire_show.php
@@ -25,6 +25,24 @@ $exemplaires = $bdd->query($commande)->fetchAll();
 $commande = "SELECT * FROM OEUVRE WHERE OEUVRE.noOeuvre = ".$bdd->quote($numOeuvre).";";
 $oeuvre = $bdd->query($commande)->fetch();
 
+$commande = "SELECT EMPRUNT.noExemplaire, EMPRUNT.dateRendu , EXEMPLAIRE.noOeuvre FROM EMPRUNT 
+              INNER JOIN EXEMPLAIRE ON EMPRUNT.noExemplaire = EXEMPLAIRE.noExemplaire
+             WHERE EXEMPLAIRE.noOeuvre = ".$oeuvre["noOeuvre"]." ;";
+$emprunts = $bdd->query($commande)->fetchAll();
+
+$commande2 = "SELECT AUTEUR.nomAuteur, OEUVRE.titre, OEUVRE.noOeuvre, OEUVRE.dateParution
+	              , COUNT(E1.noExemplaire) AS nbr
+	              , COUNT(E2.noexemplaire) AS restant
+	              FROM OEUVRE
+	              JOIN AUTEUR ON AUTEUR.idAuteur = OEUVRE.idAuteur
+	              LEFT JOIN EXEMPLAIRE E1 ON E1.noOeuvre = OEUVRE.noOeuvre
+	              LEFT JOIN EXEMPLAIRE E2 ON E2.noExemplaire = E1.noExemplaire
+	                  AND E2.noExemplaire NOT IN (SELECT EMPRUNT.noExemplaire FROM EMPRUNT WHERE EMPRUNT.dateRendu IS NULL)
+	              WHERE AUTEUR.idAuteur = ".$numOeuvre."
+	              GROUP BY OEUVRE.noOeuvre
+	              ORDER BY AUTEUR.nomAuteur ASC, OEUVRE.titre ASC;";
+$exemplairesDispos = $bdd->query($commande2)->fetchAll()[0];
+
 // affichage de la vue
 ?>
 <?php include("v_head.php");  ?>
@@ -35,6 +53,7 @@ $oeuvre = $bdd->query($commande)->fetch();
 
 <div class="row">
     <div class="titreMenu">Exemplaires de l'oeuvre '<?php echo($oeuvre["titre"]) ?>' </div>
+    <div class="titreMenu">Nombre d'exemplaire(s): <?php echo($exemplairesDispos["nbr"]) ?> ,  Restant(s): <?php echo($exemplairesDispos["restant"]) ?> </div>
     <table border="2">
         <caption>Liste des exemplaires</caption>
         <thead>
@@ -44,7 +63,18 @@ $oeuvre = $bdd->query($commande)->fetch();
             </tr>
         </thead>
         <tbody>
-
+        <?php for ( $i = 0 ; $i < count($exemplaires) ; $i++): ?>
+                <tr>
+                    <td><?php echo($exemplaires[$i]["noExemplaire"]) ?></td>
+                    <td><?php echo($exemplaires[$i]["etat"]) ?></td>
+                    <td><?php echo($exemplaires[$i]["dateAchat"]) ?></td>
+                    <td><?php echo($exemplaires[$i]["prix"]) ?></td>
+                    <td>
+                        <a href="Exemplaire_edit.php?idToEdit=<?php echo($exemplaires[$i]["noExemplaire"]) ?>">Modifier</a>
+                        <a href="Exemplaire_delete.php?idToDel=<?php echo($exemplaires[$i]["noExemplaire"]) ?>">Supprimer</a>
+                    </td>
+                </tr>
+        <?php endfor; ?>
         </tbody>
     </table>
 </div>
