88db64209b463febbf7f0f16e759a17ea5c6da09
Nijtus <47044864+Nijtus@users.noreply.github.com>
2019-05-16 08:57:25 +0200

    Emprunt, premier jet

--- Emprunt_add.php
+++ Emprunt_add.php
@@ -1,16 +1,32 @@
 <?php
-include("connexion_bdd.php");
-// traitement
-if(isset($_POST)  )  // si il existe certaines variables dans le tableau associatif $_POST
-{                    // le formulaire vient d'être soumis
+include ("connexion_bdd.php");
 
+$commande = "SELECT * FROM ADHERENT ;";
+$adherent = $bdd->query($commande)->fetchAll();
+
+if (isset($_POST)){
+  
 }
-
-// affichage de la vue
 ?>
-<?php include("v_head.php");  ?>
+<?php include ("v_head.php");  ?>
 <?php include ("v_nav.php");  ?>
 
-<!-- affichage(vue) relatif à la page -->
+<div class="row">
+  <form action="#" method="post">
+    <fieldset>
+      <label></label>
+      <select name="adherent">
+          <option value="0">--Veuillez selectionnez un adherent--</option>
+          <?php foreach ($adherent as $ligne): ?>
+              <option value="<?php echo($ligne["idAdherent"]);?>">
+                  <?php echo($ligne["nomAdherent"]);?>
+              </option>
+          <?php endforeach; ?>
+      </select>
+      <input type="submit" value="Valider">
+    </fieldset>
+  </form>
+  <!-- <a href="Emprunt_show.php">Retour</a> -->
+</div>
 
 <?php include ("v_foot.php");  ?>
