cbf0c5454836c1bf65c725a439e61bc2fb7d3299
Nijtus <47044864+Nijtus@users.noreply.github.com>
2019-05-21 08:35:33 +0200

    adherent presque complet

--- Adherent_show.php
+++ Adherent_show.php
@@ -21,34 +21,33 @@ include ("fonctionsUtiles.php")
       $chaine_SQL2="DELETE FROM ADHERENT WHERE idAdherent=".$supr.";";
       $suprRes= $bdd->exec($chaine_SQL2);
     }
-    $chaine_SQL="SELECT * FROM ADHERENT;";
-    $chaine_SQL3="SELECT ADHERENT.idAdherent, EMPRUNT.dateEmprunt, COUNT(EMPRUNT.idAdherent) AS gounter
+    // $chaine_SQL="SELECT * FROM ADHERENT;";
+    $chaine_SQL="SELECT ADHERENT.idAdherent, ADHERENT.nomAdherent
+                  , ADHERENT.adresse , ADHERENT.datePaiement
+                  , EMPRUNT.dateEmprunt, COUNT(EMPRUNT.idAdherent) AS gounter
                   FROM ADHERENT
                   LEFT JOIN EMPRUNT ON EMPRUNT.idAdherent = ADHERENT.idAdherent
-                  GROUP BY EMPRUNT.idAdherent;";
+                  GROUP BY ADHERENT.idAdherent;
+                  ORDER BY ADHERENT.idAdherent";
 
     $reponse= $bdd->query($chaine_SQL);
     $donnee = $reponse->fetchAll();
-    $emp = $bdd->query($chaine_SQL3);
-    $compte= $emp->fetchAll();
-    // header("Location: Etudiant_show_result.php");
+
+
 
 
   ?>
 
   <table border="1">
-    <th>id</th><th>nom</th><th>adresse</th><th>date paiment</th>
+    <th>id</th><th>nom</th><th>adresse</th><th>Emprunt</th><th>date paiment</th>
     <?php foreach($donnee as $row): ?>
         <?php
             echo "<tr><td>".$row['idAdherent']."</td><td>".$row['nomAdherent'].
-            "</td><td>".$row['adresse']."</td><td>".$row['datePaiement'].
+            "</td><td>".$row['adresse']."</td><td>Nombre d'emprunt :".$row['gounter']."</td><td>".$row['datePaiement'].
             "</td><td><a href='Adherent_show.php?suprimer=".$row['idAdherent'].
             "'>Suprimer</a></td><td><a href='Adherent_edit.php?idToEdit=".$row['idAdherent']."'>Modifier</a></td>";?>
     <?php endforeach;?>
   </table>
 
-  <?php foreach ($compte as $row) {
-    echo $row['idAdherent']." nbr= ".$row['gounter']."<br>";
-  };?>
 </div>
 <?php include ("v_foot.php");  ?>
