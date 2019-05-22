a5fd7535c462a958c03c54859526b06ec27b5587
Dhovabear <43112786+dhovabear@users.noreply.github.com>
2019-05-22 15:49:51 +0200

    Fin de Exemplaire delete

--- Exemplaire_delete.php
+++ Exemplaire_delete.php
@@ -5,3 +5,11 @@
  * Date: 16/05/19
  * Time: 08:33
  */
+
+include "connexion_bdd.php";
+
+if(isset($_GET["idToDel"]) && isset($_GET["noOeuvre"])){
+    $commande = "DELETE FROM EXEMPLAIRE WHERE EXEMPLAIRE.noExemplaire = ".$_GET["idToDel"];
+    $res = $bdd->exec($commande);
+    header("Location: Exemplaire_show.php?noOeuvre=".$_GET["noOeuvre"]."&delSuc=".$res);
+}
--- Exemplaire_show.php
+++ Exemplaire_show.php
@@ -15,6 +15,9 @@ $numOeuvre = 0;
 $smtWasAdd = false;
 $addSuc = 0;
 
+$smtWasDel = false;
+$delSuc = 0;
+
 $nbrOeuvr = 0;
 if(isset($_GET)){
     if(isset($_GET["noOeuvre"])){
@@ -23,6 +26,10 @@ if(isset($_GET)){
     if(isset($_GET["addSuc"])){
          $smtWasAdd = true;
          $addSuc = $_GET["addSuc"];
+    }
+    if(isset($_GET["delSuc"])){
+        $smtWasDel = true;
+        $delSuc = $_GET["delSuc"];
     }
 }
 
@@ -67,6 +74,8 @@ foreach ($exemplaires as $ligne){
     <div class="titreMenu">Nombre d'exemplaire(s): <?php echo($oeuvrInfo["nbr"]) ?> ,  Restant(s): <?php echo $nbrOeuvr ?> </div>
 
     <br>
+    <a href="Oeuvre_show.php">Retour</a><br>
+    <br>
     <a href="Exemplaire_add.php?idOeuvre=<?php echo($numOeuvre) ?>">Ajouter un exemplaire</a>
     <?php if($smtWasAdd): ?>
             <?php if($addSuc == 1): ?>
@@ -76,41 +85,55 @@ foreach ($exemplaires as $ligne){
             <?php endif; ?>
             <div class="alert" style="color:green"></div>
     <?php endif; ?>
+    <?php if($smtWasDel): ?>
+        <?php if($delSuc == 1): ?>
+            <div class="alert" style="color: green">Suppression réussie !</div>
+        <?php else: ?>
+            <div class="erreur" style="color:red;">Erreur lors de la suppression!</div>
+        <?php endif; ?>
+        <div class="alert" style="color:green"></div>
+    <?php endif; ?>
+
+    <?php if(isset($exemplaires[0])): ?>
     <table border="2">
         <caption>Liste des exemplaires</caption>
         <thead>
-            <tr>
-                <th>N exemplaire</th><th>etat</th>
-                <th>date achat</th><th>prix</th><th>opérations</th>
-            </tr>
+        <tr>
+            <th>N exemplaire</th><th>etat</th>
+            <th>date achat</th><th>prix</th><th>opérations</th>
+        </tr>
         </thead>
         <tbody>
         <?php foreach($exemplaires as $ligne): ?>
                 <?php
-                    $style = "";
-                    $isDispo = "(indisponible) ";
-                    $commande = "SELECT EXEMPLAIRE.noExemplaire NOT IN(SELECT EMPRUNT.noExemplaire FROM EMPRUNT WHERE EMPRUNT.dateRendu = '0000-00-00' OR EMPRUNT.dateRendu IS NULL) AS dispo FROM EXEMPLAIRE
+        $style = "";
+        $isDispo = "(indisponible) ";
+        $commande = "SELECT EXEMPLAIRE.noExemplaire NOT IN(SELECT EMPRUNT.noExemplaire FROM EMPRUNT WHERE EMPRUNT.dateRendu = '0000-00-00' OR EMPRUNT.dateRendu IS NULL) AS dispo FROM EXEMPLAIRE
                                  INNER JOIN OEUVRE ON OEUVRE.noOeuvre = EXEMPLAIRE.noOeuvre
                                  WHERE OEUVRE.noOeuvre = ".$numOeuvre." AND EXEMPLAIRE.noExemplaire = ".$ligne["noExemplaire"].";";
-                    $res = $bdd->query($commande)->fetch();
-                    if($res["dispo"]){
-                        $style = "background-color:lime;";
-                        $isDispo = "(disponible) ";
-                    }
-                ?>
-                <tr style="<?php echo($style) ?>">
-                    <td><?php echo($ligne["noExemplaire"]) ?>   <?php echo($isDispo) ?></td>
-                    <td><?php echo($ligne["etat"]) ?></td>
-                    <td><?php echo($ligne["dateAchat"]) ?></td>
-                    <td><?php echo($ligne["prix"]) ?></td>
-                    <td>
-                        <a href="Exemplaire_edit.php?idToEdit=<?php echo($ligne["noExemplaire"]) ?>">Modifier</a>
-                        <a href="Exemplaire_delete.php?idToDel=<?php echo($ligne["noExemplaire"]) ?>">Supprimer</a>
-                    </td>
-                </tr>
+        $res = $bdd->query($commande)->fetch();
+        if($res["dispo"]){
+        $style = "background-color:lime;";
+        $isDispo = "(disponible) ";
+        }
+        ?>
+        <tr style="<?php echo($style) ?>">
+            <td><?php echo($ligne["noExemplaire"]) ?>   <?php echo($isDispo) ?></td>
+            <td><?php echo($ligne["etat"]) ?></td>
+            <td><?php echo($ligne["dateAchat"]) ?></td>
+            <td><?php echo($ligne["prix"]) ?></td>
+            <td>
+                <a href="Exemplaire_edit.php?idToEdit=<?php echo($ligne["noExemplaire"]) ?>">Modifier</a>
+                <a href="Exemplaire_delete.php?idToDel=<?php echo($ligne["noExemplaire"]) ?>&noOeuvre=<?php echo $oeuvre["noOeuvre"]?>">Supprimer</a>
+            </td>
+        </tr>
         <?php endforeach; ?>
         </tbody>
     </table>
+    <?php else: ?>
+        <br><br>
+            <div class="titreMenu"> Aucun exemplaire disponible !</div>
+    <?php endif; ?>
 </div>
 
 
